48d28cf7def1b459697cf8cdaf3757fd2feee1d2
soojin.park <soojin.park@datamaker.io>
2022-07-29 23:18:53 +0900

    try: 리액트에서 어떻게 threejs 랜더링을 할지 고민중

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
+import * as THREE from 'three'
 
 function App() {
+  const threeCanvas= useRef<HTMLCanvasElement>(null);
+  useEffect(()=>{
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
+    camera.position.set(0,0,50)
+    const canvas= threeCanvas.current
+    if(!canvas)return
+    const renderer = new THREE.WebGLRenderer({canvas})
+    const boxGeometry = new THREE.BoxGeometry(10,10,10)
+    const boxMaterial = new THREE.MeshNormalMaterial()
+    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial)
+    scene.add(boxMesh)
+    const animate = () =>{
+      boxMesh.rotation.x += 0.01;
+      boxMesh.rotation.y += 0.01;
+      renderer.render(scene,camera)
+      window.requestAnimationFrame(animate)
+    }
+    animate()
+  })
   return (
     <div className="App">
-      <header className="App-header">
-       <h1 className="text-3xl font-bold underline">hello</h1>
-      </header>
+      <canvas id="threeCanvas" ref={threeCanvas}/>
     </div>
   );
 }
